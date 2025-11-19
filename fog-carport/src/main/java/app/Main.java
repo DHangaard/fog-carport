d886f948e0ecedb0b6113c9a2dbb1be2172e7f67
DHangaard <124468537+DHangaard@users.noreply.github.com>
2025-11-19 11:33:34 +0100

    Project structure created

--- fog-carport/src/main/java/app/Main.java
+++ fog-carport/src/main/java/app/Main.java
@@ -1,20 +1,39 @@
 package app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+import java.util.logging.Logger;
+
 public class Main
 {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    private static final String USER = System.getenv("JDBC_USER") != null ?
+            System.getenv("JDBC_USER") : "postgres";
+
+    private static final String PASSWORD = System.getenv("JDBC_PASSWORD") != null ?
+            System.getenv("JDBC_PASSWORD") : "postgres";
+
+    private static final String URL = System.getenv("JDBC_CONNECTION_STRING") != null ?
+            System.getenv("JDBC_CONNECTION_STRING") : "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+
+    private static final String DB = System.getenv("JDBC_DB") != null ?
+            System.getenv("JDBC_DB") : "carport";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
     public static void main(String[] args)
     {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++)
+        Javalin app = Javalin.create(config ->
         {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            config.staticFiles.add("/public");
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+            config.staticFiles.add("/templates");
+        }).start(7070);
+
+        app.get("/", ctx -> ctx.render("/index"));
     }
 }
