c291390ea022f497748427169f9f8373ab661b92
DHangaard <124468537+DHangaard@users.noreply.github.com>
2025-11-20 11:46:33 +0100

    Material: Refactored constructor and generated method generateDescription. dimension attributes changed int -> Integer to make these nullable

--- src/main/java/app/entities/Material.java
+++ src/main/java/app/entities/Material.java
@@ -1,25 +1,60 @@
 package app.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
 @Data
 public class Material
 {
     private int materialId;
-    private int materialVariantId;
     private String name;
-    private double unitPrice;
-
-    private Enum materialType;
-    private Enum materialCategory;
-
+    private MaterialCategory materialCategory;
+    private MaterialType materialType;
+    private Integer materialWidth;
+    private Integer materialHeight;
     private String unit;
     private String usage;
+    private Integer materialVariantId;
+    private double unitPrice;
+    private int variantLength;
     private String description;
 
-    private int variantLength;
-    private int materialWidth;
-    private int materialHeight;
+    Material(int materialId,
+             String name,
+             MaterialCategory materialCategory,
+             MaterialType materialType,
+             Integer materialWidth,
+             Integer materialHeight,
+             String unit,
+             String usage,
+             Integer materialVariantId,
+             double unitPrice,
+             int variantLength)
+    {
+        this.materialId = materialId;
+        this.name = name;
+        this.materialCategory = materialCategory;
+        this.materialType = materialType;
+        this.materialWidth = materialWidth;
+        this.materialHeight = materialHeight;
+        this.unit = unit;
+        this.usage = usage;
+        this.materialVariantId = materialVariantId;
+        this.unitPrice = unitPrice;
+        this.variantLength = variantLength;
+
+        this.description = generateDescription();
+    }
+
+    private String generateDescription()
+    {
+        StringBuilder description = new StringBuilder();
+        if (this.materialWidth != null && this.materialHeight != null)
+        {
+            description.append(materialWidth).append("x");
+            description.append(materialHeight).append(" mm. ");
+        }
+        description.append(name);
+        return description.toString();
+    }
+
 }
