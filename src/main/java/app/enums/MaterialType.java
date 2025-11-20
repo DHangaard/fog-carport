03b1fb71ab7eb7532448b3d03324385c72b8ae31
DHangaard <124468537+DHangaard@users.noreply.github.com>
2025-11-20 12:06:21 +0100

    ENUM MaterialType added

---
+++ src/main/java/app/enums/MaterialType.java
@@ -0,0 +1,21 @@
+package app.enums;
+
+public enum MaterialType
+{
+    POST("Stolpe"),
+    RAFTER("Spær"),
+    ROOF("Tag"),
+    BOARD("Bræt"),
+    FASTENER("Skrue"),
+    FITTING("Beslag");
+
+    private final String displayCategory;
+
+    MaterialType(String displayCategory) {
+        this.displayCategory = displayCategory;
+    }
+
+    public String getDisplayCategory() {
+        return displayCategory;
+    }
+}
