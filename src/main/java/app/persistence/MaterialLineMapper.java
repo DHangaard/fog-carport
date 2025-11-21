7add05e81ca3feb5ab888fdd943cf3e7ecae2f1a
DHangaard <124468537+DHangaard@users.noreply.github.com>
2025-11-21 09:47:19 +0100

    MaterialLineMapper: Constructor created

--- src/main/java/app/persistence/MaterialLineMapper.java
+++ src/main/java/app/persistence/MaterialLineMapper.java
@@ -1,5 +1,23 @@
 package app.persistence;
 
+import app.entities.Material;
+import app.exceptions.DatabaseException;
+
 public class MaterialLineMapper
 {
+    private ConnectionPool connectionPool;
+    private MaterialMapper materialMapper;
+
+    public MaterialLineMapper(ConnectionPool connectionPool)
+    {
+        this.connectionPool = connectionPool;
+        this.materialMapper = new MaterialMapper(connectionPool);
+    }
+
+    public MaterialLine createMaterialLine(int bomId, int materialId, int quantity, double lineTotal) throws DatabaseException
+    {
+        
+    }
+
+
 }
