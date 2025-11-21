72456e7fee8f3a00037918952f27bbb87beadd03
DHangaard <124468537+DHangaard@users.noreply.github.com>
2025-11-21 10:38:36 +0100

    MaterialMapper: Method skeletons added

--- src/main/java/app/persistence/MaterialLineMapper.java
+++ src/main/java/app/persistence/MaterialLineMapper.java
@@ -1,7 +1,13 @@
 package app.persistence;
 
 import app.entities.Material;
+import app.entities.MaterialLine;
 import app.exceptions.DatabaseException;
+
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class MaterialLineMapper
 {
@@ -14,10 +20,42 @@ public class MaterialLineMapper
         this.materialMapper = new MaterialMapper(connectionPool);
     }
 
-    public MaterialLine createMaterialLine(int bomId, int materialId, int quantity, double lineTotal) throws DatabaseException
+    public MaterialLine createMaterialLine(MaterialLine materialLine) throws DatabaseException
     {
-        
+        String sql = """
+                INSERT INTO material_line (bom_id, material_id, quantity, line_total)
+                VALUES  (?, ?, ?, ?)
+                RETURNING material_line_id
+                """;
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, materialLine.getBomId());
+            ps.setInt(2, materialLine.getMaterial().getMaterialId());
+            ps.setInt(3, materialLine.getQuantity());
+            ps.setDouble(4, materialLine.getLineTotal());
+            // METHOD BODY NOT FINISHED!
+        }
+        catch (SQLException)
+        {
+            throw new DatabaseException("");
+        }
+        return null;
     }
 
+    public MaterialLine getMaterialLineById(MaterialLine materialLine) throws DatabaseException
+    {
+        return null;
+    }
 
+    public boolean updateMaterialLine(MaterialLine materialLineId) throws DatabaseException
+    {
+        return false;
+    }
+
+    public boolean deleteMaterialLine(int materialLineId) throws DatabaseException
+    {
+        return false;
+    }
 }
