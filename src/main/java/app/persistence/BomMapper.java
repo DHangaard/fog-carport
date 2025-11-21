fbed87a8f4a24dbbf45e0092b7675a47f9327351
Morten Jensen <mo_ten@hotmail.com>
2025-11-21 10:35:44 +0100

    Added skeleton to BomMapper

--- src/main/java/app/persistence/BomMapper.java
+++ src/main/java/app/persistence/BomMapper.java
@@ -1,12 +1,38 @@
 package app.persistence;
 
+import app.entities.BillOfMaterials;
+
 public class BomMapper
 {
     private ConnectionPool connectionPool;
+    private MaterialLineMapper materialLineMapper;
+
 
     public BomMapper(ConnectionPool connectionPool)
     {
         this.connectionPool = connectionPool;
+        this.materialLineMapper = new MaterialMapper(connectionPool);
+    }
+
+    public BillOfMaterials createBillOfMaterials(BillOfMaterials billOfMaterials)
+    {
+        //billOfMaterials.getMaterialLines().forEach(materialLine -> materialLine);
+        return null;
+    }
+
+    public BillOfMaterials getBillOfMaterialsById(int offerId)
+    {
+        return null;
+    }
+
+    public boolean updateBillOfMaterials(BillOfMaterials billOfMaterials)
+    {
+        return false;
+    }
+
+    public boolean deleteBillOfMaterials(int bomId)
+    {
+        return false;
     }
 
 
