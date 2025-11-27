641ae0ecd6174ae086bc6c03fab6747a823e5b26
Morten Jensen <mo_ten@hotmail.com>
2025-11-27 11:28:48 +0100

    Implemented calculateNumberOfBracketScrews in bomservice

--- src/main/resources/sql/sql-pieces-per-unit.sql
+++ src/main/resources/sql/sql-pieces-per-unit.sql
@@ -21,3 +21,15 @@ INSERT INTO material_variant (
 ) VALUES (
              (SELECT material_id FROM material WHERE name = 'hulbånd'), 1000, 239.75, 1
          );
+
+INSERT INTO material (
+    name, category, type, material_width, material_height, unit, usage
+) VALUES (
+             'Beslagskruer', 'FITTINGS_AND_FASTENERS', 'FASTENER', null, null, 'Pakke', 'Til	montering	af	universalbeslag	+	hulbånd	'
+         );
+
+INSERT INTO material_variant (
+    material_id, variant_length, unit_price, pieces_per_unit
+) VALUES (
+             (SELECT material_id FROM material WHERE name = 'Beslagskruer'), 5, 189.75, 250
+         );
