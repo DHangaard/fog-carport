4daaf2485afcf191a814bb6b8c31d73bd4315cb4
Morten Jensen <mo_ten@hotmail.com>
2025-11-29 14:44:55 +0100

    Added implementation in services classes and templates on sendgrid.com

--- src/main/java/app/services/IEmailService.java
+++ src/main/java/app/services/IEmailService.java
@@ -1,10 +1,11 @@
 package app.services;
 
 import app.dto.UserDTO;
+import app.entities.OrderDetail;
 
 public interface IEmailService
 {
     boolean sendRequestConfirmation(UserDTO user);
     boolean sendOfferReady(UserDTO user);
-    boolean sendOrderConfirmation(UserDTO user);
+    boolean sendOrderConfirmation(UserDTO user, OrderDetail orderDetail);
 }
--- src/main/java/app/services/SendGridEmailService.java
+++ src/main/java/app/services/SendGridEmailService.java
@@ -1,6 +1,7 @@
 package app.services;
 
 import app.dto.UserDTO;
+import app.entities.OrderDetail;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,9 +9,9 @@ import java.util.Map;
 public class SendGridEmailService implements IEmailService
 {
     private SendGridClient sendGridClient;
-    private final String REQUEST_CONFIRMATION_TEMPLATE = "d-ae86cc47271143398d6064a7e7de3d4f";
-    private final String OFFER_READY_TEMPLATE = "d-OFFER_READY";
-    private final String ORDER_CONFIRMATION_TEMPLATE = "d-ORDER_CONFIRMATION";
+    private final String REQUEST_CONFIRMATION_TEMPLATE = "d-2f92a4eacaea449b99167a35094140c5";
+    private final String OFFER_READY_TEMPLATE = " d-a16e0ee9ed34411ca0acee887bfdb22a";
+    private final String ORDER_CONFIRMATION_TEMPLATE = " d-ae86cc47271143398d6064a7e7de3d4f";
 
     public SendGridEmailService()
     {
@@ -21,7 +22,7 @@ public class SendGridEmailService implements IEmailService
     @Override
     public boolean sendRequestConfirmation(UserDTO user)
     {
-        Map<String,Object> dynamicData = buildDynamicData(user);
+        Map<String,Object> dynamicData = buildDynamicUserData(user);
 
         if(!hasDataToSend(dynamicData))
         {
@@ -34,16 +35,30 @@ public class SendGridEmailService implements IEmailService
     @Override
     public boolean sendOfferReady(UserDTO user)
     {
-        return false;
+        Map<String,Object> dynamicData = buildDynamicUserData(user);
+
+        if(!hasDataToSend(dynamicData))
+        {
+            return false;
+        }
+
+        return sendGridClient.sendMail(user.email(), OFFER_READY_TEMPLATE, dynamicData);
     }
 
     @Override
-    public boolean sendOrderConfirmation(UserDTO user)
+    public boolean sendOrderConfirmation(UserDTO user, OrderDetail orderDetail)
     {
-        return false;
+        Map<String,Object> dynamicData = buildDynamicOrderAndUserData(user, orderDetail);
+
+        if(!hasDataToSend(dynamicData))
+        {
+            return false;
+        }
+        
+        return sendGridClient.sendMail(user.email(), ORDER_CONFIRMATION_TEMPLATE, );
     }
 
-    private Map<String, Object> buildDynamicData(UserDTO user)
+    private Map<String, Object> buildDynamicUserData(UserDTO user)
     {
         Map<String, Object> dynamicData = new HashMap<>();
 
@@ -64,6 +79,32 @@ public class SendGridEmailService implements IEmailService
         }
 
         return dynamicData;
+    }
+
+    private Map<String, Object> buildDynamicOrderAndUserData(UserDTO user, OrderDetail orderDetail)
+    {
+        Map<String, Object> dynaimicData = buildDynamicUserData(user);
+
+        if (orderDetail == null)
+        {
+            return dynaimicData;
+        }
+
+        dynaimicData.put("orderId", orderDetail.getOrderId());
+
+        if (orderDetail.getCarport() != null)
+        {
+            dynaimicData.put("carportLength", orderDetail.getCarport().getLength());
+            dynaimicData.put("carportWidth", orderDetail.getCarport().getWidth());
+        }
+
+        if (orderDetail.getPricingDetails() != null)
+        {
+            dynaimicData.put("pricingWithoutVat", orderDetail.getPricingDetails().getPriceWithoutVat());
+            dynaimicData.put("totalPrice", orderDetail.getPricingDetails().getTotalPrice());
+        }
+
+        return dynaimicData;
     }
 
     private boolean hasDataToSend(Map<String, Object> dynamicData)
