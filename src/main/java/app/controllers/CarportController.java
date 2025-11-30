87c5bfef59f00e93baae815597245e6dfaa4aea2
Morten Jensen <mo_ten@hotmail.com>
2025-11-30 11:10:10 +0100

    Styled carport-offer-request and build method to fill form option values

--- src/main/java/app/controllers/CarportController.java
+++ src/main/java/app/controllers/CarportController.java
@@ -4,6 +4,9 @@ import app.services.ICarportService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarportController
 {
     private ICarportService carportService;
@@ -16,11 +19,40 @@ public class CarportController
     public void addRoutes(Javalin app)
     {
         app.get("/carporte", ctx -> showBuildCarportPage(ctx));
+        app.get("/carport-formular", ctx -> showCarportFormular(ctx));
 
+    }
+
+    private void showCarportFormular(Context ctx)
+    {
+        List<Integer> carportWidthDimensions = getDimensionFromTo(240, 600, 30);
+        ctx.attribute("carportWidths", carportWidthDimensions);
+
+        List<Integer> carportLengthDimensions = getDimensionFromTo(240, 780, 30);
+        ctx.attribute("carportLengths", carportLengthDimensions);
+
+        List<Integer> shedWidthDimensions = getDimensionFromTo(210, 720, 30);
+        ctx.attribute("shedWidths", shedWidthDimensions);
+
+        List<Integer> shedLengthDimensions = getDimensionFromTo(150, 690, 30);
+        ctx.attribute("shedLengths", shedLengthDimensions);
+
+        ctx.render("request-offer-carport");
     }
 
     private void showBuildCarportPage(Context ctx)
     {
         ctx.render("build-carport");
+    }
+
+    private List<Integer> getDimensionFromTo(int from, int to, int increment)
+    {
+        List<Integer> dimensions = new ArrayList<>();
+
+        for(int i = from; i <= to; i+= increment)
+        {
+            dimensions.add(i);
+        }
+        return dimensions;
     }
 }
