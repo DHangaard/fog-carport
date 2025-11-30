c0a59709bb813569b13b7bfef499438e3fe54352
Morten Jensen <mo_ten@hotmail.com>
2025-11-30 15:37:40 +0100

    CarportController implementation started

--- src/main/java/app/controllers/CarportController.java
+++ src/main/java/app/controllers/CarportController.java
@@ -1,8 +1,14 @@
 package app.controllers;
 
+import app.entities.Carport;
+import app.entities.Shed;
+import app.enums.RoofType;
+import app.enums.ShedPlacement;
+import app.exceptions.DatabaseException;
 import app.services.ICarportService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +27,50 @@ public class CarportController
         app.get("/carporte", ctx -> showBuildCarportPage(ctx));
         app.get("/carport-formular", ctx -> showCarportFormular(ctx));
 
+        app.post("/request-offer", ctx -> handleCarportRequest(ctx));
+    }
+
+    private void handleCarportRequest(Context ctx)
+    {
+        String carportWidthString = ctx.formParam("carportWidth");
+        String carportLengthString = ctx.formParam("carportLength");
+        String roofType = ctx.formParam("roofType");
+        String shedWidthString = ctx.formParam("shedWidth");
+        String shedLengthString = ctx.formParam("shedLength");
+        String customerNote = ctx.formParam("customerNote");
+
+        Carport carport;
+        Shed shed;
+
+        try
+        {
+            int carportWidth = Integer.parseInt(carportWidthString);
+            int carportLength = Integer.parseInt(carportLengthString);
+
+            if(!shedLengthString.equals("NONE") && shedWidthString.equals("NONE"))
+            {
+                int shedWidth = Integer.parseInt(shedWidthString);
+                int shedLength = Integer.parseInt(shedLengthString);
+                shed = new Shed(0, shedLength, shedWidth, ShedPlacement.FULL_WIDTH);
+            }
+
+            carport = new Carport(0, carportWidth, carportLength, RoofType.FLAT, shed);
+
+            carportService.validateCarport(carport);
+
+
+        }
+        catch (NumberFormatException e)
+        {
+            ctx.attribute("errorMessage", "Mål skal være et tal");
+            ctx.redirect("request-offer-carport");
+        }
+
+        catch (IllegalArgumentException e)
+        {
+            ctx.attribute("errorMessage", e.getMessage());
+            ctx.redirect("request-offer-carport");
+        }
     }
 
     private void showCarportFormular(Context ctx)
--- src/main/java/app/services/CarportService.java
+++ src/main/java/app/services/CarportService.java
@@ -1,5 +1,44 @@
 package app.services;
 
+import app.entities.Carport;
+import app.entities.Shed;
+
 public class CarportService implements ICarportService
 {
+    @Override
+    public void validateCarport(Carport carport)
+    {
+        int carportWidth = carport.getWidth();
+        int carportLength = carport.getLength();
+
+        if (carportWidth < 240 || carportWidth > 600)
+        {
+            throw new IllegalArgumentException("Carport bredde skal være mellem 240 og 600 cm");
+        }
+
+        if (carportLength < 240 || carportLength > 780)
+        {
+            throw new IllegalArgumentException("Carport længde skal være mellem 240 og 780 cm");
+        }
+
+
+        Shed shed = carport.getShed();
+        if (shed != null)
+        {
+            if (shed.getWidth() <= 0 || shed.getLength() <= 0)
+            {
+                throw new IllegalArgumentException("Skuret skal have både bredde og længde");
+            }
+
+            if (shed.getWidth() > carportWidth)
+            {
+                throw new IllegalArgumentException("Skurets bredde må ikke være større end carportens bredde");
+            }
+
+            if (shed.getLength() > carportLength)
+            {
+                throw new IllegalArgumentException("Skurets længde må ikke være større end carportens længde");
+            }
+        }
+    }
 }
