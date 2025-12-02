40f71365bd48d3eaf3e83ed072527428f344cf9b
DHangaard <124468537+DHangaard@users.noreply.github.com>
2025-12-03 00:59:23 +0100

    Implemented AppProperties methods i main on DB variables

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,6 +7,7 @@ import app.persistence.ConnectionPool;
 import app.persistence.UserMapper;
 import app.persistence.ZipCodeMapper;
 import app.services.*;
+import app.util.AppProperties;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
@@ -17,16 +18,16 @@ public class Main
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     private static final String USER = System.getenv("JDBC_USER") != null ?
-            System.getenv("JDBC_USER") : "postgres";
+            System.getenv("JDBC_USER") : AppProperties.getRequired("db.user");
 
     private static final String PASSWORD = System.getenv("JDBC_PASSWORD") != null ?
-            System.getenv("JDBC_PASSWORD") : "postgres";
+            System.getenv("JDBC_PASSWORD") : AppProperties.getRequired("db.password");
 
     private static final String URL = System.getenv("JDBC_CONNECTION_STRING") != null ?
-            System.getenv("JDBC_CONNECTION_STRING") : "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+            System.getenv("JDBC_CONNECTION_STRING") : AppProperties.getRequired("db.url");
 
     private static final String DB = System.getenv("JDBC_DB") != null ?
-            System.getenv("JDBC_DB") : "carport";
+            System.getenv("JDBC_DB") : AppProperties.getRequired("db.dbname");
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
@@ -37,7 +38,7 @@ public class Main
             config.staticFiles.add("/public");
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
             config.staticFiles.add("/templates");
-        }).start(7070);
+        }).start(AppProperties.getRequiredInt("server.port"));
 
         UserMapper userMapper = new UserMapper(connectionPool);
         ZipCodeMapper zipCodeMapper = new ZipCodeMapper(connectionPool);
