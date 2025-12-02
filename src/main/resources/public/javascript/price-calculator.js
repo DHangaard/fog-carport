4c342c9f183994816f321e9e47cf44581bbfc029
Morten Jensen <mo_ten@hotmail.com>
2025-12-02 11:39:08 +0100

    Added javascript to update price in real time

---
+++ src/main/resources/public/javascript/price-calculator.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const totalInput = document.getElementById("totalPriceInput");
+    const coverageInput = document.getElementById("coverageInput");
+
+    const costPrice = parseFloat(totalInput.dataset.costprice);
+    const originalTotal = parseFloat(totalInput.dataset.original);
+
+    function update()
+    {
+        const total = parseFloat(totalInput.value) || 0;
+
+        const priceWithoutVat = (total / 1.25);
+        const priceWithVat = total;
+        const coverage = ((priceWithoutVat - costPrice) / priceWithoutVat) * 100;
+        const diff = total - originalTotal;
+
+        document.getElementById("priceWithOutVatDisplay").textContent = priceWithoutVat.toFixed(2);
+        document.getElementById("costPriceDisplay").textContent = costPrice.toFixed(2);
+        document.getElementById("coverageDisplay").textContent = coverage.toFixed(1);
+        document.getElementById("priceWithoutVatDisplay2").textContent = priceWithoutVat.toFixed(2);
+        document.getElementById("priceWithVatDisplay").textContent = priceWithVat.toFixed(2);
+        document.getElementById("differenceDisplay").textContent = diff.toFixed(2);
+
+        coverageInput.value = coverage.toFixed(1);
+    }
+
+    totalInput.addEventListener("input", update);
+    update();
+});
