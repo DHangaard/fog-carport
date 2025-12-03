9fb181b7820ecb686bbdfe30f6617d294acdaf39
Morten Jensen <mo_ten@hotmail.com>
2025-12-03 16:59:53 +0100

    Added skeleton for controlling seller views Tilbud og Ordre

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,7 +1,10 @@
 package app.controllers;
 
+import app.dto.UserDTO;
+import app.enums.Role;
 import app.services.IOrderService;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 
 public class OrderController
 {
@@ -14,6 +17,32 @@ public class OrderController
 
         public void addRoutes(Javalin app)
         {
+            app.get("/offers", ctx -> showOfferOverview(ctx));
+            app.get("/orders", ctx -> showOrderOverview(ctx));
 
         }
+
+    private void showOrderOverview(Context ctx)
+    {
+        if(!userIsAdmin(ctx)){return;}
+    }
+
+    private void showOfferOverview(Context ctx)
+    {
+        if(!userIsAdmin(ctx)){return;}
+    }
+
+    private boolean userIsAdmin(Context ctx)
+    {
+        UserDTO userDTO = ctx.sessionAttribute("currentUser");
+
+        if(userDTO == null)
+        {
+            ctx.sessionAttribute("errorMessage", "Du skal logge ind for at tilgå denne side");
+            ctx.redirect("/login");
+            return false;
+        }
+
+        return userDTO.role().equals(Role.SALESREP);
+    }
 }
