13e7c7c73140cb428da253af9e2a9b83694e3eea
Morten Jensen <mo_ten@hotmail.com>
2025-12-04 14:02:21 +0100

    Added method in MaterialLineMapper to update quantity

---
+++ src/main/java/app/services/MaterialService.java
@@ -0,0 +1,26 @@
+package app.services;
+
+import app.exceptions.DatabaseException;
+import app.persistence.MaterialLineMapper;
+
+public class MaterialService implements IMaterialService
+{
+    private MaterialLineMapper materialLineMapper;
+
+    public MaterialService(MaterialLineMapper materialLineMapper)
+    {
+        this.materialLineMapper = materialLineMapper;
+    }
+
+    @Override
+    public boolean updateBillOfMaterialLineQuantity(int materialLineId, int quantity) throws DatabaseException
+    {
+        return materialLineMapper.updateMaterialLineQuantity(materialLineId, quantity);
+    }
+
+    @Override
+    public boolean deleteBillOfMaterialLine(int materialLineId) throws DatabaseException
+    {
+        return false;
+    }
+}
