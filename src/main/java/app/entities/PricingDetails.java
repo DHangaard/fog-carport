9706491335d2bfa17fd2b6ec07c3ef382a3a85df
DHangaard <124468537+DHangaard@users.noreply.github.com>
2025-12-08 12:56:13 +0100

    Moved price formatting methods from PricingDetails -> PriceFormatUtil

--- src/main/java/app/entities/PricingDetails.java
+++ src/main/java/app/entities/PricingDetails.java
@@ -13,7 +13,6 @@ public class PricingDetails
     private final double VAT_PERCENTAGE = 0.25;
     private double vatAmount;
     private double totalPrice;
-    private static final Locale DANISH_LOCALE = new Locale("da", "DK");
 
     public PricingDetails(double costPrice, double coveragePercentage)
     {
@@ -22,35 +21,5 @@ public class PricingDetails
         this.priceWithoutVat = costPrice / (1 - coveragePercentage / 100.0);
         this.vatAmount = priceWithoutVat * VAT_PERCENTAGE;
         this.totalPrice = priceWithoutVat + vatAmount;
-    }
-
-    public String getFormattedCostPrice()
-    {
-        return String.format(DANISH_LOCALE, "%,.2f", costPrice);
-    }
-
-    public String getFormattedPriceWithoutVat()
-    {
-        return String.format(DANISH_LOCALE, "%,.2f", priceWithoutVat);
-    }
-
-    public String getFormattedTotalPrice()
-    {
-        return String.format(DANISH_LOCALE, "%,.2f", totalPrice);
-    }
-
-    public String getFormattedVatAmount()
-    {
-        return String.format(DANISH_LOCALE, "%,.2f", vatAmount);
-    }
-
-    public String getFormattedCoveragePercentage()
-    {
-        return String.format(DANISH_LOCALE, "%.1f", coveragePercentage);
-    }
-
-    public String getFormattedProfit()
-    {
-        return String.format(DANISH_LOCALE, "%,.2f", priceWithoutVat - costPrice);
     }
 }
